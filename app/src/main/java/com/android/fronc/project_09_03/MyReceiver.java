8e4621c1f9df62dcf552ab030b78f848d9593fc9
fronczek7 <fronczek7f@gmail.com>
2018-12-19 21:25:25 +0100

    Testowanie sendOrderedBroadcast

--- app/src/main/java/com/android/fronc/project_09_03/MyReceiver.java
+++ app/src/main/java/com/android/fronc/project_09_03/MyReceiver.java
@@ -3,6 +3,7 @@ package com.android.fronc.project_09_03;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
@@ -11,29 +12,42 @@ public class MyReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        Handler handler = new Handler();
+        Resources resources = context.getResources();
+
+        final String message = intent.getStringExtra("com.android.fronc.project_09_01.EXTRA_MESSAGE_TEXT");
+        final String appName = resources.getString(R.string.app_name);
+
         int resultCode = getResultCode();
         String resultData = getResultData();
         Bundle resultExtras = getResultExtras(true);
-        String stringExtra = resultExtras.getString("stringExtra");
 
-        resultCode++;
-        stringExtra += "->OR3";
+        final String stringExtra = resultExtras.getString("stringExtra");
 
-        final String toastText = "OR3\n" +
-                "resultCode: " + resultCode + "\n" +
-                "resultData: " + resultData + "\n" +
-                "stringExtra: " + stringExtra;
-
-        Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
-                Toast.makeText(context, toastText, Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "Broadcast detected " + appName + " (" + message + ")", Toast.LENGTH_LONG).show();
             }
         }, 4000);
 
-        resultData = "OR3";
-        resultExtras.putString("stringExtra", stringExtra);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Toast.makeText(context, "extra string: " + stringExtra, Toast.LENGTH_LONG).show();
+            }
+        }, 8000);
 
+        final String stringFeedback = stringExtra + " " + appName + ";";
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Toast.makeText(context, "Send feedback string: " + stringFeedback, Toast.LENGTH_LONG).show();
+            }
+        }, 12000);
+
+        resultData = "OR3";
+        resultExtras.putString("stringExtra", stringFeedback);
+
+        resultCode++;
         setResult(resultCode, resultData, resultExtras);
     }
 }
